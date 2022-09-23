0c23f7f5dd928dd480e1303f3bc7dc630909a014
Tatiana Tatarinova <loontik22@gmail.com>
2022-09-23 22:20:28 +0300

    error Chrome Ubuntu fixed

--- src/main/java/app/pages/AbstractPage.java
+++ src/main/java/app/pages/AbstractPage.java
@@ -2,9 +2,11 @@ package app.pages;
 
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriverException;
 
 public abstract class AbstractPage {
     protected String pageUrl;
+    private final static int MAX_RETRY_COUNT = 5;
 
     public AbstractPage(String pageUrl) {
         this.pageUrl = pageUrl;
@@ -12,6 +14,35 @@ public abstract class AbstractPage {
 
     @Step("Переход на главную страницу приложения")
     public void open() {
-        Selenide.open(pageUrl);
+        int retryCount = 0;
+        while(true)
+        {
+            try
+            {
+                Selenide.open(pageUrl);
+                break;
+            }
+            catch(WebDriverException e)
+            {
+                if( retryCount > MAX_RETRY_COUNT )
+                {
+                    throw new RuntimeException("Too many retries...", e);
+                }
+
+//                logger.warn("encountered exception : ", e);
+//                logger.warn("Trying again...");
+
+                retryCount++;
+                try
+                {
+                    Thread.sleep(2_000);
+                }
+                catch (InterruptedException interruptedException)
+                {
+                    interruptedException.printStackTrace();
+                }
+                continue;
+            }
+        }
     }
 }
